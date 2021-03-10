0f7f7f1788723197988716f153b1889c20383dc0
AhmadMor <Atrosa@ymail.com>
2021-03-10 19:13:24 +0330

    Convert HeaderTop to a dynamic component

--- components/header/HeaderTop.js
+++ components/header/HeaderTop.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 
 
@@ -19,16 +20,30 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-export default function HeaderTop(props) {
+const menuList = [
+    {id: "45timvnzjcy", title: "HOME", path: "/", isActive: false,},
+    {id: "vqz81agpn9n", title: "SHOP", path: "#", isActive: false,},
+    {id: "jg7xmh1dvbd", title: "RECIPES", path: "/recipes", isActive: true,},
+    {id: "q8mxrl47qwq", title: "ABOUT", path: "#", isActive: false,}
+];
 
+
+export default function HeaderTop(props) {
     const classes = useStyles();
+
+    const listItems = menuList.map(menu => (
+        <Link
+            href={menu.path}
+            key={menu.id}
+            variant="subtitle2"
+            className={menu.isActive ? classes.active : ''}
+        >{menu.title}</Link>
+    ));
+
     return (
         <Box pb={1} pt={3} {...props}>
-            <Typography className={classes.root} >
-                <Link href="/" variant="subtitle2">HOME</Link>
-                <Link href="#" variant="subtitle2">SHOP</Link>
-                <Link href="/recipes" variant="subtitle2" className={classes.active}>RECIPES</Link>
-                <Link href="#" variant="subtitle2">ABOUT</Link>
+            <Typography className={classes.root}>
+                {listItems}
             </Typography>
         </Box>
     );
